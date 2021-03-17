f3acd63f312c8a1f47853edd5d140adc8104d42e
yavorant <yavorant@gmail.com>
2021-03-17 16:13:07 +0200

    function computerPlay

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log('script linked');
+
+//function computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
+function computerPlay() {
+    const computerChoiceList = ["Rock", "Paper", "Scissors"];
+    return computerChoiceList[Math.floor(Math.random() * Math.floor(3))]    
+  }
+
+  console.log(computerPlay())  
