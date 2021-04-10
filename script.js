b8bd3cec3cba1e8fc7475f071109864c8af1b2a8
yavorant <yavorant@gmail.com>
2021-04-10 12:45:51 +0300

    style

--- script.js
+++ script.js
@@ -5,10 +5,10 @@
 
 function computerPlay() {
     const computerChoiceList = ["rock", "paper", "scissors"];
-    return computerChoiceList[Math.floor(Math.random() * 3)]    
+    return computerChoiceList[Math.floor(Math.random() * 3)]
   }
 
-//function humanPlay that will takes a case-insensitive input from an user's prompt  
+//function humanPlay that will takes a case-insensitive input from an user's prompt
 
 function humanPlay() {
   //The toLowerCase() method returns the calling string value converted to lower case
@@ -19,8 +19,8 @@ function humanPlay() {
       break;
     }
     alert("Please try again!");
-  } 
-  return usersInput    
+  }
+  return usersInput
 }
 
 /*
@@ -34,10 +34,10 @@ function humanPlay() {
   "You Lose! Scissors cuts paper"   //playerSelection = "paper" &  computerSelection = "scissors"
   */
 
-// a function that plays a single round of Rock Paper Scissors. The function should take two parameters 
+// a function that plays a single round of Rock Paper Scissors. The function should take two parameters
 // playerSelection and computerSelection - and then return a string that declares the winner of the round
-// using ternary operator and 9 cases, first 3 equals cases, and than you could win or loose by choosing every 
-// one of 3 cases (6 more) 
+// using ternary operator and 9 cases, first 3 equals cases, and than you could win or loose by choosing every
+// one of 3 cases (6 more)
 
 
 function playRound(playerSelection, computerSelection) {
@@ -45,33 +45,44 @@ function playRound(playerSelection, computerSelection) {
          : playerSelection == "rock" &&  computerSelection == "scissors" ? "You Win! Rock crushes scissors"
          : playerSelection == "scissors" &&  computerSelection == "rock" ? "You Lose! Rock crushes scissors"
          : playerSelection == "paper" &&  computerSelection == "rock" ? "You Win! Paper covers rock"
-         : playerSelection == "rock" &&  computerSelection == "paper" ? "You Lose! Paper covers rock" 
-         : playerSelection == "scissors" &&  computerSelection == "paper" ? "You Win! Scissors cuts paper" 
-         : "You Lose! Scissors cuts paper";  
+         : playerSelection == "rock" &&  computerSelection == "paper" ? "You Lose! Paper covers rock"
+         : playerSelection == "scissors" &&  computerSelection == "paper" ? "You Win! Scissors cuts paper"
+         : "You Lose! Scissors cuts paper";
 }
 
 let playerSelection;
 let computerSelection;
 
-//console.log("human", playerSelection); 
-//console.log("AI:", computerSelection); 
+//console.log("human", playerSelection);
+//console.log("AI:", computerSelection);
 //console.log(playRound(playerSelection, computerSelection));
 
 
-// a function game(). 
-// use the playRound() function inside of this one to play a 5 round game 
+// a function game().
+// use the playRound() function inside of this one to play a 5 round game
 // that keeps score and reports a winner or loser at the end.
-//you should be using console.log() to display the results of each round 
+//you should be using console.log() to display the results of each round
 //and the winner at the end.
-//a function game() 
+//a function game()
 
 function game() {
   for (let round = 1; round <= 5 ; round++) {
     playerSelection = humanPlay();
     computerSelection = computerPlay();
     let oneRoundGame = playRound(playerSelection, computerSelection);
-    console.log(`round # ${round}: Human = ${playerSelection} & AI = ${computerSelection} | ${oneRoundGame} |`);   
+    console.log(`round # ${round}: Human = ${playerSelection} & AI = ${computerSelection} | ${oneRoundGame} |`);
   }
 }
 
-game();
+//game();
+
+/*
+1) Copy your original code into a new file so you don’t lose it.
+2) For now, remove the logic that plays exactly five rounds.
+3) Create three buttons, one for each selection.
+Add an event listener to the buttons that calls your playRound function
+with the correct playerSelection every time a button is clicked. (you can keep the console.
+  logs for this step)
+4) Add a div for displaying results and change all of your console.logs into DOM methods.
+5) Display the running score, and announce a winner of the game once one player reaches 5 points.
+*/
